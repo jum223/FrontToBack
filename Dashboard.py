89d0e6ac71fb522b42716a248d12e6d7b3e01b0d
Juan Mozos <jum223@lehigh.edu>
2023-05-01 22:41:50 -0400

    grg

--- Dashboard.py
+++ Dashboard.py
@@ -51,7 +51,7 @@ def main():
             data_filtered = data.query("Industry in @option1")
             
         sns.set_style("whitegrid", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
-        sns.despine()
+        sns.despine(top=True, right=True)
 
         # Create plot
         if option1 or (all_option1 in option1):
